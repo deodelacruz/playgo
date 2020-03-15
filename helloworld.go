fab136f9a7a61c45a9b9b525d76a1ee06858838e
Rodil Dela Cruz <rdelacruz@salesforce.com>
2020-03-14 21:08:04 -0700

    personal go playground

---
+++ helloworld.go
@@ -0,0 +1,26 @@
+//package github.com/deodelacruz
+
+package main
+
+import (
+  "fmt" 
+  //"reflect"
+  "strconv"
+)
+
+func main() {
+  //var b bool = true
+  var s string = "1"
+  //var i int = 4
+  //var f float64 = 2.3
+
+  //fmt.Println(s, strconv.ParseInt(s))
+}
+
+func sayHello(friend string) string {
+   return ("Hello " + friend)
+}
+
+func sum(a,b int) int {
+  return a+b
+}
