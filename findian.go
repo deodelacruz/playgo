88c1b436f79f0478b538292891b4453a82e1383f
Rodil Dela Cruz <rdelacruz@salesforce.com>
2020-03-15 20:07:12 -0700

    recover findian.go

--- findian.go
+++ findian.go
@@ -1,8 +1,23 @@
 package main
 
-//"fmt"
-//"strings"
+import (
+	"fmt"
+	"os"
+	"strings"
+)
 
 func main() {
+	fmt.Println("Hi. Please input a string, and I'll say 'Found!' if it starts with the character ‘i’,\n" + "ends with the character ‘n’, and contains the character ‘a’.")
 
+	var userInput string
+	if _, err := fmt.Scanln(&userInput); err != nil {
+		os.Exit(1)
+	} else {
+		s := strings.ToLower(userInput)
+		if strings.HasPrefix(s, "i") && strings.HasSuffix(s, "n") && strings.Contains(s, "a") {
+			fmt.Println("Found!")
+		} else {
+			fmt.Println("Not Found!")
+		}
+	}
 }
