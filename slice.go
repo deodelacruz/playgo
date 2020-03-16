339d17ebc42e130c0cb12ea172c26c79726f0872
Rodil Dela Cruz <rdelacruz@salesforce.com>
2020-03-15 19:25:59 -0700

    working slice.go

---
+++ slice.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"sort"
+	"strconv"
+)
+
+func main() {
+	sli := make([]int, 3)
+
+	for {
+
+		fmt.Println("Please enter an integer to add to the slice. Press 'X' to exit.")
+
+		var userInput string
+		if _, err := fmt.Scanln(&userInput); err != nil {
+			os.Exit(1)
+		} else {
+
+			if userInput == "X" {
+				break
+			}
+
+			parsedInt, _ := strconv.ParseInt(userInput, 0, 64)
+			sli = append(sli, int(parsedInt))
+			sort.Ints(sli)
+			fmt.Printf("Sorted slice: %v\n", sli)
+		}
+	}
+
+	return
+}
+
+/*
+Write a program which prompts the user to enter integers and stores the integers in a sorted slice. The program should be written as a loop. Before entering the loop, the program should create an empty integer slice of size (length) 3. During each pass through the loop, the program prompts the user to enter an integer to be added to the slice. The program adds the integer to the slice, sorts the slice, and prints the contents of the slice in sorted order. The slice must grow in size to accommodate any number of integers which the user decides to enter. The program should only quit (exiting the loop) when the user enters the character ‘X’ instead of an integer.
+*/
