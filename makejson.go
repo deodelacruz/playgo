b2754a6a65837fd406b626040860c90829cb8431
Rodil Dela Cruz <rdelacruz@salesforce.com>
2020-03-21 13:20:57 -0700

    updated

--- makejson.go
+++ makejson.go
@@ -1,5 +1,9 @@
 /*
-Write a program which prompts the user to first enter a name, and then enter an address. Your program should create a map and add the name and address to the map using the keys “name” and “address”, respectively. Your program should use Marshal() to create a JSON object from the map, and then your program should print the JSON object.
+Write a program which prompts the user to first enter a name, and then enter an
+address. Your program should create a map and add the name and address to the
+map using the keys “name” and “address”, respectively. Your program should use
+Marshal() to create a JSON object from the map, and then your program should
+print the JSON object.
 */
 
 package main
@@ -13,7 +17,6 @@ import (
 
 func main() {
 
-	// Ask for name, and address
 	reader := bufio.NewReader(os.Stdin)
 
 	fmt.Println("Hi. What is your name?")
@@ -27,18 +30,9 @@ func main() {
 	map1 := make(map[string]string)
 	map1["name"] = name
 	map1["address"] = address
-
-	fmt.Printf("Map Person: %v\n", map1)
+	//fmt.Printf("Map Person: %v\n", map1)
 
 	json1, _ := json.Marshal(map1)
 	fmt.Printf("Json Person: %v\n", string(json1))
 
-	person1 := person{name, address}
-	fmt.Printf("Struct Person: %v\n", person1)
-
-}
-
-type person struct {
-	name    string
-	address string
 }
