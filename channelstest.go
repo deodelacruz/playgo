519b88366dafae5a0675e82942940d6ccbdbd9f8
Rodil Dela Cruz <rdelacruz@salesforce.com>
2020-04-11 16:07:55 -0700

    working channels test

--- channelstest.go
+++ channelstest.go
@@ -2,15 +2,36 @@ package main
 
 import (
 	"fmt"
+	"sync"
 )
 
+var wg sync.WaitGroup
+var stringCh chan string
+
 func main() {
-	for i := 0; i < 2; i++ {
-		readFromChan(i)
+	maxThreads := 2
+	stringCh = make(chan string)
+	// spawn thread that writes to channel
+	go writeToChan()
+	// spwan multiple threads that read from channel
+	for i := 0; i < maxThreads; i++ {
+		wg.Add(1)
+		go readFromChan(i)
 	}
+	wg.Wait()
 	fmt.Println("Done with main().")
 }
 
+func writeToChan() {
+	sli := []string{"a", "b"}
+	for _, elem := range sli {
+		stringCh <- elem
+	}
+	close(stringCh)
+}
+
 func readFromChan(threadId int) {
-	fmt.Printf("Thread %v\n", threadId)
+	val := <-stringCh
+	fmt.Printf("Thread%v read from channel this value: %s\n", threadId, val)
+	wg.Done()
 }
