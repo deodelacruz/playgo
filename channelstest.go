7dd4560e3962fe0265ff1695c72bb594d95bf635
Rodil Dela Cruz <rdelacruz@salesforce.com>
2020-04-11 19:54:32 -0700

    updated

--- channelstest.go
+++ channelstest.go
@@ -1,7 +1,13 @@
+/*
+Major problem here is that there are 2 threads that write to the same channel.
+When does writing thread close the channel?
+*/
+
 package main
 
 import (
 	"fmt"
+	"strconv"
 	"sync"
 )
 
@@ -9,29 +15,55 @@ var wg sync.WaitGroup
 var stringCh chan string
 
 func main() {
-	maxThreads := 5
+	maxReadThreads := 2
+	maxWriteThreads := 2
 	stringCh = make(chan string)
-	// spawn thread that writes to channel
-	go writeToChan()
-	// spwan multiple threads that read from channel
-	for i := 0; i < maxThreads; i++ {
+
+	// spawn thread that write to channel
+	for i := 0; i < maxWriteThreads; i++ {
+		wg.Add(1)
+		go writeToChan(i)
+	}
+
+	// spawn multiple threads that read from channel
+	for i := 0; i < maxReadThreads; i++ {
 		wg.Add(1)
 		go readFromChan(i)
 	}
+
 	wg.Wait()
+	close(stringCh)
 	fmt.Println("Done with main().")
 }
 
-func writeToChan() {
+/*
+func provideSessionChannel() chan string { //provide channel back to requestor for comms
+	return make(chan str)
+} */
+
+func writeToChan(threadId int) {
 	sli := []string{"a", "b"}
 	for _, elem := range sli {
-		stringCh <- elem
+		elemStr := strconv.Itoa(threadId) + elem
+		fmt.Printf("WriteThread%v attempting channel send: %v\n", threadId, elemStr)
+		select {
+		case stringCh <- elemStr:
+			// won't be reached until something is read from ch
+			fmt.Printf("WriteThread%v successfully sent to channel: %v\n", threadId, elemStr)
+		default:
+			fmt.Printf("WriteThread%v was not able to send to channel: %v\n", threadId, elemStr)
+		}
 	}
-	close(stringCh)
 }
 
 func readFromChan(threadId int) {
-	val := <-stringCh
-	fmt.Printf("Thread%v read from channel this value: %s\n", threadId, val)
+	fmt.Printf("ReadThread%v attempting to read from channel.\n", threadId)
+	select {
+	case val := <-stringCh:
+		// won't be reached until something is read from ch
+		fmt.Printf("ReadThread%v successfully read from channel: %v\n", threadId, val)
+	default:
+		fmt.Printf("ReadThread%v was not able to read from channel.\n", threadId)
+	}
 	wg.Done()
 }
