c75561db799b5c88e8c2285b2df6d38c4b94e167
Rodil Dela Cruz <rdelacruz@salesforce.com>
2020-08-11 10:42:15 -0700

    updated

--- webservice/controllers/user.go
+++ webservice/controllers/user.go
@@ -1,9 +1,21 @@
 package controller
 
-import "net/http"
+import (
+	"net/http"
+	"regexp"
+)
 
-type userController struct{}
+type userController struct {
+	userIDPattern *regexp.Regexp
+}
 
 func (uc userContoller) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte{"Hello from the User Controller"})
+}
+
+func newUserController() *userController {
+	return &userController{
+		userIDPattern: regexp.MustCompile(`^/users/(\d+)/?`),
+	}
+
 }
